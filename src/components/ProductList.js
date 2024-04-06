bfa234e8b30c1ba2064249766e23940c9f6f238e
Karkhanis, Vinayak <vinayak.karkhanis@nuance.com>
2024-04-06 22:26:53 +0530

    Using hook

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,10 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
+import { useFetch } from "./hooks/useFetch";
 
 export const ProductList = () => {
 
-    const [products, setProducts] = useState([]);
     const [url, setUrl] = useState('http://localhost:8000/products');
-
-    const fetchProducts = useCallback(async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        setProducts(data);
-    }, [url]);
-
-
-useEffect(() => {
-    fetchProducts();
-}, [fetchProducts]);
+    const { data:products } = useFetch(url);
 
 
 return (
@@ -23,7 +13,7 @@ return (
             <button onClick={() => setUrl('http://localhost:8000/products')}>All</button>
             <button onClick={() => setUrl('http://localhost:8000/products?discount=yes')}>Eligible for Discount</button>
         </div>
-        {products.map(product => (
+        {products && products.map(product => (
             <div className="card" key={product.id}>
                 <p className="id">{product.id}</p>
                 <p className="name">{product.name}</p>
---
+++ src/components/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    }
+    fetchData();
+  }, [url]);
+  return { data }
+}
