9a4567cca0d099aeb6c0bab12076d489a14ac6be
Karkhanis, Vinayak <vinayak.karkhanis@nuance.com>
2024-04-06 21:48:26 +0530

    using UseCallBack

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,34 +1,38 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export const ProductList = () => {
 
     const [products, setProducts] = useState([]);
     const [url, setUrl] = useState('http://localhost:8000/products');
 
-    useEffect(() => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => setProducts(data));
+    const fetchProducts = useCallback(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data);
     }, [url]);
 
 
+useEffect(() => {
+    fetchProducts();
+}, [fetchProducts]);
 
-    return (
-        <section>
-            <div className="filter">
-                <button onClick={() => setUrl('http://localhost:8000/products')}>All</button>
-                <button onClick={() => setUrl('http://localhost:8000/products?discount=yes')}>Eligible for Discount</button>
+
+return (
+    <section>
+        <div className="filter">
+            <button onClick={() => setUrl('http://localhost:8000/products')}>All</button>
+            <button onClick={() => setUrl('http://localhost:8000/products?discount=yes')}>Eligible for Discount</button>
+        </div>
+        {products.map(product => (
+            <div className="card" key={product.id}>
+                <p className="id">{product.id}</p>
+                <p className="name">{product.name}</p>
+                <p className="info">
+                    <span>₹{product.price}</span>
+                    <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+                </p>
             </div>
-            {products.map(product => (
-                <div className="card" key={product.id}>
-                    <p className="id">{product.id}</p>
-                    <p className="name">{product.name}</p>
-                    <p className="info">
-                        <span>₹{product.price}</span>
-                        <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
-                    </p>
-                </div>
-            ))}
-        </section>
-    )
+        ))}
+    </section>
+)
 }
